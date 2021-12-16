486dc9eb122b790f0a7004cc56b1e00484237cee
edixon control de calidad <edixonsalgueiro@gmail.com>
2021-12-16 10:52:42 -0400

    102. Numeros y Operaciones

---
+++ js/05.js
@@ -0,0 +1,17 @@
+// Numeros
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1); //residuo de la division 
+
+const cliente = "Juan";
+const cliente2 = "Pablo";
+console.log(cliente * cliente2); // Error NaN (Not a number)
