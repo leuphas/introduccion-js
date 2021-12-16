bba25c123a697c22cf4f84fc57ee3f2a47c2429b
edixon control de calidad <edixonsalgueiro@gmail.com>
2021-12-16 11:05:58 -0400

    103. El objeto Math en JS

---
+++ js/06.js
@@ -0,0 +1,18 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI; //Numero pi
+resultado = Math.round(2.7); // redondea
+resultado = Math.ceil(2.2); // redondea siempre hacia arriba
+resultado = Math.floor(2.9); // redondea siempre hacia abajo
+resultado = Math.sqrt(144); // raiz cuadrada
+resultado = Math.abs(-144); // convierte numero negativo a positivo
+resultado = Math.min(3, 5, 1, 8, 10); // minimo numero de un listado
+resultado = Math.max(3, 5, 1, 8, 10); // maximo numero de un listado
+resultado = Math.random(); // numero aleatorio
+resultado = Math.floor(Math.random()*30); //numero aleatorio entero hasta 30
+
+
+
+console.log(resultado);
