c89c4af475e913ca1051295f916d64e721e461e8
edixon control de calidad <edixonsalgueiro@gmail.com>
2021-12-16 10:04:35 -0400

    100. Strings o Cadenas de texto

---
+++ js/04.js
@@ -0,0 +1,11 @@
+// String o cadena de texto
+
+// si quieres incluir en tu string unas comillas dobles tienes que ponerlas asi \" o tambien puedes encerrrarlo entre comillas sencillas 
+const producto = "Monitor de 20\""; 
+// const producto2 = String('Monitor de 20 pulgadas'); //forma menos comun de crear un string 
+// const producto3 = new String('Monitor de 50 pulgadas'); // si creas un string de esta forma terminara siendo un objeto 
+
+// con typeof puedes saber que tipo de datos tiene una variable
+console.log(producto);
+// console.log(producto2);
+// console.log(producto3);
