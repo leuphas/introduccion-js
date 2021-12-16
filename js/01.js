8523a2eeb5b94506e75d74f050ce07ceeae33e4a
edixon control de calidad <edixonsalgueiro@gmail.com>
2021-12-16 09:01:51 -0400

    98. Variables en JavaScript

---
+++ js/01.js
@@ -0,0 +1,30 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Asignamos la variable sin valor
+
+// producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+// Inicializar multiples variables
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// Reglas para las variables
+// Las variables no pueden iniciar con numeros ni caracteres especiales
+// var 1producto;
+// var -disponible;
+
+var _disponible; // Esta si esta permitido
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase (el mas famoso y utilizado en javascript)
+var NombreProducto = 'Monitor HD'; // Pascal Case
+
+// Las variables son Case Sensitive 
+
+console.log(producto);
