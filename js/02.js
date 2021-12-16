1fbcbd3ebeda75f8d8dcfb5dae7f354975e8e485
edixon control de calidad <edixonsalgueiro@gmail.com>
2021-12-16 09:42:51 -0400

    99. variables con let y const

---
+++ js/02.js
@@ -0,0 +1,30 @@
+// Variables con let
+
+// Las variables con let pueden iniciar con un valor o asignarle un valor despues, tambien se pueden reasignar mas adelante, basicamente no hay diferencia en la estructura de creacion entre var y let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+let disponible; // Asignamos la variable sin valor
+
+// producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+// Inicializar multiples variables
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; 
+
+// Reglas para las variables
+// Las variables no pueden iniciar con numeros ni caracteres especiales
+// let 1producto;
+// let -disponible;
+
+let _disponible; // Esta si esta permitido
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Monitor HD'; // Camelcase (el mas famoso y utilizado en javascript)
+let NombreProducto = 'Monitor HD'; // Pascal Case
+
+// Las variables son Case Sensitive 
---
+++ js/03.js
@@ -0,0 +1,32 @@
+// Variables con const
+
+// No puedes inicializar una variable sin valor y tampoco se pueden reasignar
+// Cuando tengas un valor fijo que no vas a modificar utilizalo como const
+// puedes inicialar todas tus variables con let y luego ir cambiando a const las que no son modificables para mejorar tu archivo
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+const disponible = true;
+
+// producto = true; // Reasignando el valor de la variable
+
+
+
+// Inicializar multiples variables
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; 
+
+// Reglas para las variables
+// Las variables no pueden iniciar con numeros ni caracteres especiales
+// const 1producto;
+// const -disponible;
+
+const _disponible = true; // Esta si esta permitido
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Monitor HD'; // Camelcase (el mas famoso y utilizado en javascript)
+const NombreProducto = 'Monitor HD'; // Pascal Case
+
+// Las variables son Case Sensitive 
