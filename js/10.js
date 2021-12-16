3f6919bbf36d2d8043561740d5747aae9e5d7076
edixon control de calidad <edixonsalgueiro@gmail.com>
2021-12-16 14:12:19 -0400

    108. Modificar Objetos

--- js/10.js
+++ js/10.js
@@ -1,11 +1,5 @@
 // Objetos
 
-const nombreProducto = "Monitor 20 Pulgadas";
-const precio = 300;
-const disponible = true;
-
-// Con un objeto puedes fusionar las 3 variables de arriba
-
 const producto = {
     nombreProducto: "Monitor 20 Pulgadas",
     precio: 300,
@@ -15,9 +9,17 @@ const producto = {
 console.log(producto);
 
 // De esta forma accedes a la propiedad de un objeto
-console.log(producto.precio); 
-console.log(producto.nombreProducto); 
-console.log(producto.disponible); 
+// console.log(producto.precio); 
+// console.log(producto.nombreProducto); 
+// console.log(producto.disponible); 
 
 // otra forma de acceder a una propiedad
-console.log(producto["precio"]);
+// console.log(producto["precio"]);
+
+// Agregar nuevas propiedades
+producto.imagen = 'imagen.jpg';
+
+// Eliminar propiedades
+delete producto.disponible;
+
+console.log(producto);
