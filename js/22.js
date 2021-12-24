7097886e72edb8a2e0a5565118f2b41319dfd055
Edixon Gonzalez <edixonsalgueiro@gmail.com>
2021-12-24 12:03:22 -0400

    122. Estructuras de control

---
+++ js/22.js
@@ -0,0 +1,26 @@
+// const puntaje = 1000;
+
+// if (puntaje  !== 1000) {
+//     console.log('No es igual');
+// } else {
+//     console.log('El puntaje es 1000');
+// }
+
+// const efectivo = 700;
+// const carrito = 800;
+
+// if (efectivo > carrito) {
+//     console.log('El usuario puede pagar');
+// } else {
+//     console.log('Fondos insuficientes');
+// }
+
+const rol = "EDITOR"   
+
+if (rol === "ADMINISTRADOR") {
+    console.log('El rol es Administrador');
+} else if (rol === "EDITOR") {
+    console.log('El rol es Editor');
+} else{
+    console.log('No tienes acceso');
+}
